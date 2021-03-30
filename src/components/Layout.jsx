608803c4a132f2476dca8d37534a7a1cd40ee064
John Claro <jkrclaro@gmail.com>
2021-03-30 15:38:21 +0100

    Change nav color depending on pathname

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom';
 class Layout extends React.Component {
 
     render() {
+        const pathname = window.location.pathname;
         return (
             <div>
                 <div className='container mt-3 mb-5'>
                     <div className='text-center mt-3'>
                         <h1>Covid in Ireland</h1>
-                        <Link to='/' className='btn btn--template primary--bg mr-3'>Cases</Link>
-                        <Link to='/swabs' className='btn btn--template primary--bg mr-3'>Swabs</Link>
+                        <Link to='/' className={`btn btn--template ${pathname === '/' ? 'primary' : 'light'}--bg mr-3`}>Cases</Link>
+                        <Link to='/swabs' className={`btn btn--template ${pathname === '/swabs' ? 'primary' : 'light'}--bg mr-3`}>Swabs</Link>
                     </div>
                     {this.props.children}
                 </div>
