0e4cbf07ba4098142f0032d62b7e59d735de93cc
John Claro <jkrclaro@gmail.com>
2021-06-16 20:34:58 +0100

    Remove HSE alert

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -24,9 +24,6 @@ class Layout extends React.Component {
                                 {pathname === link.url ? null : ' ->'}
                             </Link>
                         )}
-                        <div className='alert alert-danger mt-3' role='alert'>
-                            Recent <a href='https://healthservice.hse.ie/staff/news/general/staff-update-hse-it-cyber-attack.html'>HSE IT Cyber Attack</a> has paused updates to GeoHive COVID-19 Hub until further notice.
-                        </div>
                     </div>
                     {this.props.children}
                 </div>
